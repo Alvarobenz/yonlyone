d577027192fa0e9991d16d13078662961bed6a5f
dougdliu <dougdliu@gmail.com>
2015-03-17 08:43:58 -0700

    Rename SelectionSort.cpp to InsertionSort.cpp

---
+++ InsertionSort.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{	// Start of function
+
+	/* Selection Sort */
+
+	int array [5] = {1, 9, 3, 2, 5};
+	int i, j, key;
+
+	cout << "Unsorted Array:" << endl;
+
+	/* Pre-Sorted Array */
+	for(int i = 0; i < 5; i++)
+	{
+		cout << array[i];
+		cout << " ";
+	}
+
+	cout << endl;
+
+	/* Insertion Sort */
+
+	for(int i = 1; i < 5; i++)
+	{
+		key = array[i];
+		j = i - 1;
+		while(j > 0 && array[j] > key)
+		{
+			array[j+1] = array[j];
+			j--;
+		}
+		array[j+1] = key;
+	}
+
+	cout << "Sorted Array:" << endl;
+
+	/* Post-Sorted Array */
+	for(int i = 0; i < 5; i++)
+	{
+		cout << array[i];
+		cout << " ";
+	}
+} // End of Function
